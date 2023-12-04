18cfb6e4a10a782be1da281660ae33a4cc25f980
MaltsevaYulia <112495890+MaltsevaYulia@users.noreply.github.com>
2023-12-04 12:27:27 +0000

    Deploying to gh-pages from @ MaltsevaYulia/test-SoftRyzen@eb3ddce627bbaea9ab1ce7782c643ece88f825a5 🚀

---
+++ static/js/546.3753523c.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunktest_softryzen=self.webpackChunktest_softryzen||[]).push([[546],{546:function(e,t,s){e.exports=s.p+"static/media/YuriiTurchenko1280px1x.5b178d5b9465952e6f5d.jpg"}}]);
