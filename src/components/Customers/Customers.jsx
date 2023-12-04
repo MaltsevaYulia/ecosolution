07b4310b71bc02f901a618e1e4cb317b3f9cf749
MaltsevaYulia <112495890+MaltsevaYulia@users.noreply.github.com>
2023-12-04 17:47:53 +0200

    cleare coments

--- src/components/Customers/Customers.jsx
+++ src/components/Customers/Customers.jsx
@@ -1,35 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { customers } from 'constants/customers';
 
 import css from './Customers.module.css';
 
 const Customers = () => {
-  const [images, setImages] = useState([]);
-
-  useEffect(() => {
-    const loadImage = async img => {
-      try {
-        const imageModule = await import(`../../assets/customers/${img}.png`);
-        setImages(prevImages => [
-          ...prevImages,
-          { img, src: imageModule.default },
-        ]);
-      } catch (error) {
-        console.error(`Error loading image: ${error}`);
-      }
-    };
-
-    customers.forEach(({ img }) => {
-      loadImage(img);
-    });
-  }, []);
+ 
 
   return (
     <section className={css.section} id="customers">
       <h2 className="title">our satisfied customers</h2>
       <ul className={css.list}>
         {customers.map(({ name, url, img, id }) => {
-          const loadedImage = images?.find(loadedImg => loadedImg.img === img);
+        
           return (
             <li key={id} className={css.item}>
               <a
@@ -39,9 +21,8 @@ const Customers = () => {
                 className={css.link}
               >
                 <p className={css.brandName}>{name}</p>
-                {loadedImage && images && (
-                  <img src={loadedImage.src} alt={name} className={css.logo} />
-                )}
+                <img src={img} alt={name} className={css.logo} />
+        
               </a>
             </li>
           );
--- src/constants/customers.js
+++ src/constants/customers.js
@@ -1,22 +1,32 @@
 import { nanoid } from 'nanoid';
 
+import zakhidnyi from 'assets/customers/zakhidnyi.png';
+import audi from 'assets/customers/audi.png';
+import dnipro from 'assets/customers/dnipro.png';
+import bosch from 'assets/customers/bosch.png';
+
 export const customers = [
-  { name: 'Bosch', img: 'bosch', url: 'https://www.bosch.com/', id: nanoid() },
+  {
+    name: 'Bosch',
+    img: bosch,
+    url: 'https://www.bosch.com/',
+    id: nanoid(),
+  },
   {
     name: 'Zakhidnyi Buh',
-    img: 'zakhidnyi',
+    img: zakhidnyi,
     url: 'https://zahbug.com/en/',
     id: nanoid(),
   },
   {
     name: 'Audi',
-    img: 'audi',
+    img: audi,
     url: 'https://www.audi.com/en.html',
     id: nanoid(),
   },
   {
     name: 'Dnipro M',
-    img: 'dnipro',
+    img: dnipro,
     url: 'https://dnipro-m.ua/',
     id: nanoid(),
   },
